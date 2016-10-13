d6b47e354c80de965f8b863bfda7eb7cae1dd677
Brian Hurst <brian.hurst@gsa.gov>
2016-10-13 15:23:47 -0400

    Alternative financial animation

--- js/main.js
+++ js/main.js
@@ -6,27 +6,4 @@ import './app/form-validation';
 import './app/form-field-format';
 import './app/idv-finance-helper';
 
-
-import { TextField } from 'field-kit';
-
-const inputs = document.querySelectorAll("[class*='floating-container'] input");
-
-for (let i = 0; i < inputs.length; i++) {
-  const parent = inputs[i].parentNode;
-  const label = parent.querySelector('label');
-  const input = new TextField(inputs[i]);
-
-  input.setFocusedPlaceholder('');
-  input.setUnfocusedPlaceholder(label.textContent);
-
-  input.setDelegate({
-    textFieldDidEndEditing: function(input) {
-      if(input.value() == ''){
-        inputs[i].classList.add('empty');
-      } else {
-        inputs[i].classList.remove('empty');
-      }
-    }
-  });
-
-}
+import './mockup/financial-form'
---
+++ js/mockup/financial-form.js
@@ -0,0 +1,34 @@
+import 'classlist.js';
+import { TextField } from 'field-kit';
+
+const fields = document.querySelectorAll("[class*='floating-container'] input");
+
+if (fields) {
+  fields.forEach(function(f) {
+    const field = new TextField(f);
+
+    field.setDelegate({
+      textFieldDidEndEditing: function(field) {
+        if(field.value() != ''){
+          f.classList.add('active');
+        } else {
+          f.classList.remove('active');
+        }
+      },
+      textFieldDidBeginEditing: function(field) {
+        fields.forEach(function(f) {
+          if(f != document.activeElement) {
+            f.classList.remove('active');
+          }
+        });  
+      },
+      textDidChange: function(field) {
+        fields.forEach(function(f) {
+          if(f != document.activeElement) {
+            f.value = '';
+          }
+        });  
+      }
+    });
+  });
+}
