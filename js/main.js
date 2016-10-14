df43b8cd0af91da0d83f1779f0e9cb9cb3592cf8
Brian Hurst <brian.hurst@gsa.gov>
2016-10-14 16:53:11 -0400

    Add cloud.gov config files

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 
-// import './app/pw-toggle';
-// import './app/pw-strength';
+import './app/pw-toggle';
+import './app/pw-strength';
 import './app/form-validation';
 import './app/form-field-format';
 import './app/idv-finance-helper';
